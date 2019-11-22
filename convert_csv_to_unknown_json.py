f5d5a9398801b933ab9f8dc83be3cd3dc76e5adb
Michael Pilosov <consistentbayes@gmail.com>
2019-11-22 14:03:16 -0700

    fewer tests.

--- convert_csv_to_unknown_json.py
+++ convert_csv_to_unknown_json.py
@@ -5,7 +5,7 @@ a = pd.read_csv('client_list.csv', header=None)
 a[0].apply(lambda x: format_str(x))
 a = a.set_index([0])
 d = a.to_dict()[1]
-with open('client_list-unknown-words.json', 'w') as f:
+with open('client_list-unknown_words.json', 'w') as f:
     json.dump(d, f)
 with open('client_list.txt', 'w') as f:
     f.write('\n'.join(list(d.keys())))
--- tests/test_spellfix.py
+++ tests/test_spellfix.py
@@ -6,12 +6,11 @@ import spellfix
 def test_script(monkeypatch):
     mkn.main() # make text file
     # test options
-    for opt in ['Q', 'S', 'K', 'U', 'L']:
-        monkeypatch.setattr('builtins.input', lambda x: 'Q')
-        spellfix.main()
-        # make sure the right files got created/saved
-        assert os.path.exists('words.txt')
-        if opt == 'S':
-            assert os.path.exists('wordlist-corrections.json')
-            assert os.path.exists('wordlist-known_words.json')
-            assert os.path.exists('wordlist-unknown_words.json')
+    monkeypatch.setattr('builtins.input', lambda x: 'Q')
+    spellfix.main()
+    # make sure the right files got created/saved
+    assert os.path.exists('words.txt')
+    if opt == 'S':
+        assert os.path.exists('wordlist-corrections.json')
+        assert os.path.exists('wordlist-known_words.json')
+        assert os.path.exists('wordlist-unknown_words.json')
